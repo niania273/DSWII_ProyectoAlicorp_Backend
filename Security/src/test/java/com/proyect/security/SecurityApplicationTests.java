4d1a89cc999a003bab83a765ced76cbee7e1db46
angiegarciaruiz <erosgabrielgarcia@gmail.com>
2025-09-25 18:49:31 -0500

    estructura basica de security

---
+++ Security/src/test/java/com/proyect/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
